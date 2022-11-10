b1d780a932295357fe871946f03289964f54128c
Shreyas Garimella <shreyas.g197@gmail.com>
2022-11-10 18:04:51 -0500

    finish style navbar

--- weather-app/src/components/Navbar.js
+++ weather-app/src/components/Navbar.js
@@ -3,17 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import './Navbar.css'
 
 function WeatherNav() {
   return (
     <Navbar bg="dark" expand="xxl" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">WeatherApp</Navbar.Brand>
+      <Container className='container'>
+        <Navbar.Brand href="#home" className='title'>WeatherApp</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Random Place</Nav.Link>
+            <Nav.Link href="#home" className='nav-link'>Home</Nav.Link>
+            <Nav.Link href="#link" className='nav-link'>Random Place</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
