cb251e2c9b87cedfee9a124a6310f515a5ebe733
Shreyas Garimella <shreyas.g197@gmail.com>
2022-11-11 16:00:09 -0500

    add fetch api and spinner

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,20 +4,29 @@ import WeatherNav from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
 
 function App() {
   const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lon, setLon] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLon(position.coords.longitude);
+      });
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
-  }, [lat, long]);
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=92abaca45f3fcc4b3c2415b155afc482`, { mode: 'cors'})
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,lon])
 
 
 
@@ -26,7 +35,8 @@ function App() {
       <WeatherNav fixed="top"/>
       <div className='main'>
         <Sidebar/>
-        <Home className="home"/>
+        {(typeof data.main != 'undefined') ? (
+         <Home className="home" weatherData={data}/>):(<Loading/>)}
       </div>
     </div>
   );
--- weather-app/src/components/Home.js
+++ weather-app/src/components/Home.js
@@ -1,16 +1,14 @@
 import Card from 'react-bootstrap/Card';
 import './Home.css'
 
-function Home() {
+function Home(props) {
   return (
     <Card style={{ width: '82%' }} className="big-card">
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{props.weatherData.name}, {props.weatherData.sys.country}</Card.Title>
+        <Card.Text>{props.weatherData.weather[0].description}</Card.Text>
+        <Card.Text>{Math.round(((props.weatherData.main.temp - 273.15) *(9/5)) + 32)}</Card.Text>
       </Card.Body>
     </Card>
   );
